1be51e0d4a8eed3207fa91957d6a242588e39f58
Ariel R <resendiz.ariel6@gmail.com>
2025-05-13 21:20:11 -0700

    added results.module.css and adjust appropriate jsx file

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,19 +1,29 @@
+import styles from "./results.module.css";
 //props received from the parent component. (name, selected, correct, onRestart)
 const Result = ({ name, selected, correct, onRestart }) => {
   //compare the selected answer with the correct answer, returns true or false.0
   const isCorrect = selected === correct;
 
   return (
-    <div>
+    <div className={styles.container}>
       {/* Display the user's name and a message corresponding to whether they got the answer correct or not. */}
-      <h2>{isCorrect ? `Great job, ${name}!` : `Nice try, ${name}.`}</h2>
+      <h2
+        className={`${styles.heading} ${
+          isCorrect ? styles.correct : styles.incorrect
+        }`}
+      >
+        {isCorrect ? `Great job, ${name}!` : `Nice try, ${name}.`}
+      </h2>
       {/* conditional rendering based on whether the answer is correct or not */}
       {!isCorrect && (
         <p>
-          The correct answer was: <strong>{correct} </strong>
+          The correct answer was:{" "}
+          <span className={styles.correctAnswer}>{correct} </span>
         </p>
       )}
-      <button onClick={onRestart}>Try Another Question</button>
+      <button className={styles.button} onClick={onRestart}>
+        Try Another Question
+      </button>
     </div>
   );
 };
